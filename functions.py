4741077e002a6ca2217dcf7f798a976f904f443c
ThomazPom <thomas.benhamou@hotmail.fr>
2020-11-15 19:55:56 +0100

    Excel working

---
+++ functions.py
@@ -0,0 +1,214 @@
+
+import datetime,math,copy
+import json
+
+import requests
+import pprint, os
+
+pp = pprint.pprint
+server = open("server.conf", "r").read()
+not_in_list = []
+qatcache = {}
+
+import xlsxwriter
+
+from openpyxl.utils.cell import get_column_letter
+
+def query_all_kraken(type,typelow,user,day_zero,sleep,extra=dict(),sort="time"):
+    import python3krakenex.krakenex.api as kapi_m
+
+    k = kapi_m.API()
+
+    k.load_key(f'{user}.key')
+
+    continue_crawl = True
+    offset = 0
+    items = []
+
+    if (os.path.exists(f"{typelow}.{user}.json")):
+        items = json.load(open(f"{typelow}.{user}.json", "r"))
+        continue_crawl = False
+    import time
+
+    while continue_crawl:
+        pp(f"query {type}")
+        crawl_part = k.query_private(type, {**extra,**{ 'start': day_zero.timestamp(),
+                                                  "end": datetime.datetime.now().timestamp(), "ofs": offset}})
+        time.sleep(sleep)
+
+        for key, val in crawl_part.get("result").get(typelow).items():
+            val[f"{typelow}_id"] = key
+            items.append(val)
+
+        continue_crawl = len(crawl_part.get("result").get(typelow).keys()) == 50
+        offset = offset + 50
+
+        print(f'{offset}/{crawl_part.get("result").get("count")}')
+
+    items.sort(key=lambda x: x.get(sort))
+    json.dump(items, open(f"{typelow}.{user}.json", "w"), indent=4)
+    return  items
+
+
+def update_balances_values(balances, item, user,timekey="time"):
+    for key, val in balances.items():
+        if key is not "ZEUR":
+            ticker = query_at(key, item.get(timekey), user)
+            if not ticker:
+                continue
+            val["price_atm"] = ticker.get("price")
+            # pp(ticker)
+            val["value_atm"] = ticker.get("price") * val["balance"]
+
+
+def workbook(things_to_export, filename="export.xlsx", stylecond=lambda x: {'bg_color': '#e8e8e8'}):
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook(filename)
+
+    worksheet = workbook.add_worksheet()
+    worksheet.set_column('A:Z', 12)
+
+    rowdecal = 2
+    worksheet.freeze_panes(rowdecal, 0)
+
+    light_blue = workbook.add_format({'color': "#FFFFFF", 'bg_color': '#4169E1'})
+    [worksheet.set_row(i, cell_format=light_blue) for i in range(2)]
+    max_seen_value_len = {
+
+    }
+    known_columns = {}
+    styles = {}
+    for idx1, val1 in enumerate(things_to_export):
+        idx1 += rowdecal
+        linestyle = stylecond(val1)
+        if not styles.get(str(linestyle)):
+            w_linestyle = workbook.add_format(linestyle)
+            w_linestyle.set_text_wrap()
+            w_linestyle.set_align('top')
+            w_linestyle.set_bottom_color("DEDEDE")
+            w_linestyle.set_bottom(5)
+            styles[str(linestyle)] = w_linestyle
+
+        worksheet.set_row(idx1, cell_format=styles[str(linestyle)])
+        for key, val2 in val1.items():
+            known_columns[key] = 1
+            idx2 = list(known_columns.keys()).index(key)
+            column_letter = get_column_letter(idx2 + 1)
+            is_iterable = type(val2) in [list, tuple]
+            max_seen_value_len[column_letter] = max(
+                max_seen_value_len.get(column_letter, 0),
+                *[len(str(x)) for x in val2] if is_iterable else [len(str(val2))], len(key)+3)
+
+            if type(val2) in [float, int]:
+                worksheet.write_number(idx1, idx2, round(val2, 2))
+                worksheet.write_formula(0, idx2, f"=SUBTOTAL(9,{column_letter}{rowdecal + 2}:{column_letter}"
+                                        + f"{len(things_to_export) + rowdecal + 2})")
+            elif is_iterable:
+                worksheet.write_string(idx1, idx2, "\r\n".join(val2))
+            else:
+                worksheet.write_string(idx1, idx2, str(val2))
+            worksheet.set_column(f"{column_letter}:{column_letter}", max_seen_value_len.get(column_letter))
+    for idx, key in enumerate(known_columns.keys()):
+        worksheet.write_string(1, idx, key)
+
+    worksheet.autofilter(rowdecal - 1, 0, len(things_to_export) + rowdecal, len(known_columns.keys()) - 1)
+    workbook.close()
+
+
+
+
+def recompose_orders(trades):
+    orders = {}
+    order_template = {
+        "refid": None,
+        "userref": None,
+        "status": "closed",
+        "reason": None,
+        "opentm": 0,
+        "closetm": math.inf,
+        "starttm": 0,
+        "expiretm": 0,
+        "descr": {
+            "pair": "XBTEUR",
+            "type": "buy",
+            "ordertype": "market",
+            "price": "0",
+            "price2": "0",
+            "leverage": "none",
+            "order": "buy 0.0 XBTEUR @ market",
+            "close": ""
+        },
+        "vol": 0,
+        "vol_exec": 0,
+        "cost": 0,
+        "fee": 0,
+        "price": 0,
+        "stopprice": 0,
+        "limitprice": 0,
+        "misc": "",
+        "oflags": "fciq",
+        "closed_id": ""
+    }
+    for trade in trades:
+        order_work = orders[trade.get("ordertxid")] = orders.get(trade.get("ordertxid"), copy.deepcopy(order_template))
+        order_work["a"] = 1
+
+
+
+
+def query_at(name, ts, user):
+    global qatcache, not_in_list
+    if name in not_in_list:
+        return False
+    if (os.path.exists(f"qatcache.{user}.json")) and len(qatcache.keys()) == 0:
+        qatcache = json.load(open(f"qatcache.{user}.json", "r"))
+    if qatcache.get(f"{name}:{ts}"):
+        return qatcache.get(f"{name}:{ts}")
+    print(f"Query Es for balance at {datetime.datetime.fromtimestamp(ts).isoformat()} and {name} (utc){datetime.datetime.utcfromtimestamp(ts).isoformat()}")
+    query = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "exists": {
+                            "field": "high"
+                        }
+                    },
+                    {
+                        "match": {
+                            "name_iso": name
+                        }
+                    }
+                    ,
+                    {
+                        "range": {
+                            "insert_date": {
+                                "gte": datetime.datetime.utcfromtimestamp(ts).isoformat()
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+        , "sort": [
+            {"insert_date": {"order": "asc"}}
+        ],
+        "size": 1
+    }
+
+    r = requests.post(f"{server}/cryptowatch-*/_search", headers={
+        'content-type': 'application/json',
+    },
+                      data=json.dumps(query))
+
+    result = r.json().get("hits").get("hits");
+    if len(result) == 0:
+        print("No result for :" + name)
+        not_in_list.append(name)
+        return False
+    else:
+        result = result[0].get("_source")
+    qatcache[f"{name}:{ts}"] = result
+    json.dump(qatcache, open(f"qatcache.{user}.json", "w"), indent=4)
+    return result
+# query_at("XXBT",datetime.datetime.now().timestamp()-10,"")
